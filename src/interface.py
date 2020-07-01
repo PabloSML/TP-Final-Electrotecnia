7c078485b43db47fe6ec0a634af799754283c7ec
pablosml <psmolkin@itba.edu.ar>
2020-07-01 16:34:52 -0300

    borre .py de /designer

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,4 @@
 # Qt Modules
-#from src.ui.mainwindow import Ui_MainWindow
 from src.ui.mainwindow import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
